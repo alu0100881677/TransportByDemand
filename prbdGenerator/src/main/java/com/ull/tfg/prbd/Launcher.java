32f9497e54d20fff03d3a017213525ea024cf1a5
Guillermo <alu0100881677@ull.edu.es>
2018-06-06 23:37:43 +0100

    actualizado Launcher

--- prbdGenerator/src/main/java/com/ull/tfg/prbd/Launcher.java
+++ prbdGenerator/src/main/java/com/ull/tfg/prbd/Launcher.java
@@ -12,11 +12,12 @@ public class Launcher {
 			System.exit(-1);
 		}
 		if(args[0].equals("-h")) {
-			System.out.println("Para ejecutar este programa debe de pasar 4 argumentos:"
-					+ "\t - nombre que recibirá el fichero"
-					+ "\t - número de paradas del problema a resolver"
-					+ "\t - número que representa el paso del tiempo entre petición y petición, se aconseja 150"
-					+ "\t - número de milisegundos entre la generación de cada petición (5 segundos = 5000)");
+			System.out.println("Para ejecutar este programa debe de pasar 4 argumentos:\n"
+					+ "\t - nombre que recibirá el fichero\n"
+					+ "\t - número de paradas del problema a resolver\n"
+					+ "\t - número que representa el paso del tiempo entre petición y petición, se aconseja 150\n"
+					+ "\t - número de milisegundos entre la generación de cada petición (5 segundos = 5000)\n");
+			System.exit(-1);
 		}
 		else if(args.length != 4) {
 			System.out.println("Ejecuteeeeee el programa con la opción -h para mostrar la ayuda");

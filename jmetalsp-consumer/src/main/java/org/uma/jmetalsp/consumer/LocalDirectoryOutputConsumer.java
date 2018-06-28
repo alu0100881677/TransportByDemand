8f8ab6491a1c475e626b0b99356f2bc61f461f31
Guillermo <alu0100881677@ull.edu.es>
2018-06-28 01:14:27 +0100

    quitando ficheros de directorioSalida

--- jmetalsp-consumer/src/main/java/org/uma/jmetalsp/consumer/LocalDirectoryOutputConsumer.java
+++ jmetalsp-consumer/src/main/java/org/uma/jmetalsp/consumer/LocalDirectoryOutputConsumer.java
@@ -88,16 +88,16 @@ public class LocalDirectoryOutputConsumer<S extends Solution<?>> implements
     List<S> solutionList = (List<S>)data.getData().get("solutionList") ;
     new SolutionListOutput(solutionList)
             .setSeparator("\t")
-            .setFunFileOutputContext(new DefaultFileOutputContext(outputDirectoryName + "/OBJ" + fileCounter + ".tsv"))
-            .setVarFileOutputContext(new DefaultFileOutputContext(outputDirectoryName + "/VAR" + fileCounter + ".tsv"))
+            //.setFunFileOutputContext(new DefaultFileOutputContext(outputDirectoryName + "/OBJ" + fileCounter + ".tsv"))
+            //.setVarFileOutputContext(new DefaultFileOutputContext(outputDirectoryName + "/VAR" + fileCounter + ".tsv"))
             .print();
-    GeneradorFrentePareto x = new GeneradorFrentePareto();
+    /*GeneradorFrentePareto x = new GeneradorFrentePareto();
     try {
 		x.generarFichero(outputDirectoryName + "/OBJ" + fileCounter + ".tsv", outputDirectoryName + "/FUN" + fileCounter + ".tsv");
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
-	}
+	}*/
     fileCounter++;
   }
 }
